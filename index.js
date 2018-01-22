2648429b446ec32df4362cbb06c69df0315b252f
Kaitlyn Dodds <kaitlyndodds@Admins-MacBook-Pro.local>
2018-01-22 11:33:07 -0800

    Added view engine and basic templates

--- index.js
+++ index.js
@@ -2,8 +2,32 @@ const express = require('express');
 const app = express();
 
 
-app.get('/', function(req, res) {
-    res.send('Hello, world!');
+// set view engine
+app.set('view engine', 'ejs');
+
+
+// INDEX (GET) - /dogs
+app.get('/dogs', function(req, res) {
+    res.render('landing');
 });
 
+// NEW (GET) - /dogs/new
+
+
+// CREATE (POST) - /dogs
+
+
+// SHOW (GET) - /dogs/:id
+
+
+// EDIT (GET) - /dogs/:id/edit
+
+
+// UPDATE (PUT) - /dogs/:id
+
+
+// DESTROY (DELETE) - /dogs/:id
+
+
+// App Port
 app.listen(3000, () => console.log('App listening on port 3000'));
